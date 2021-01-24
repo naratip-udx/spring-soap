3d7dc3d9a6cb6ff686b1615b9014d2e962afd91d
Naratip Udomsilp <nara_pua@hotmail.com>
2021-01-24 16:22:57 +0700

    :tada: init

---
+++ consuming-web-service/src/main/java/com/github/naratip_udx/consuming_web_service/ConsumingWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.naratip_udx.consuming_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingWebServiceApplication.class, args);
+	}
+
+}
---
+++ consuming-web-service/src/test/java/com/github/naratip_udx/consuming_web_service/ConsumingWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.naratip_udx.consuming_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
